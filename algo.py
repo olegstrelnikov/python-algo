fa63412f8ef0a7726b389761e1cc90492f4beb3f
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2020-03-22 04:51:34 +0400

    enhanced quick_sort() with custom pivot choose

--- algo.py
+++ algo.py
@@ -1,10 +1,19 @@
-def quick_sort(A, start=0, end=None):
+def get_start_element(A, start, end):
+    return A[start]
+
+def get_last_element(A, start, end):
+    return A[end - 1]
+
+def get_middle_element(A, start, end):
+    return A[(start + end)//2]
+
+def quick_sort(A, pivot_fn=get_start_element, start=0, end=None):
     if end == None:
         end = len(A)
     assert(start <= end)
     if end - start <= 1:
         return
-    pivot = A[start]
+    pivot = pivot_fn(A, start, end)
     left = start
     right = end - 1
     while True:
@@ -36,30 +45,26 @@ def quick_sort(A, start=0, end=None):
     assert(A[left] == pivot)
     assert(A[right] == pivot)
     assert(left < right + 1)
-    quick_sort(A, start, left)
-    quick_sort(A, right + 1, end)
+    quick_sort(A, pivot_fn, start, left)
+    quick_sort(A, pivot_fn, right + 1, end)
 
-A = [4,3,6,5,2,8,9,5,3,7,6,3,5,8,1,7,5,9,4,5,8,9,3,4,7,8,8,3,5,6,8,9,7]
-print(A)
-quick_sort(A)
-print(A)
-A = [1,2,3,4,5,6,7,8,9]
-print(A)
-quick_sort(A)
-print(A)
-A = [8,7,6,5,4,3,2,1]
-print(A)
-quick_sort(A)
-print(A)
-A = [25]
-print(A)
-quick_sort(A)
-print(A)
-A = []
-print(A)
-quick_sort(A)
-print(A)
-A = [5,5,5,5]
-print(A)
-quick_sort(A)
-print(A)
+unsorted_arrays = [
+    [4,3,6,5,2,8,9,5,3,7,6,3,5,8,1,7,5,9,4,5,8,9,3,4,7,8,8,3,5,6,8,9,7],
+    [1,2,3,4,5,6,7,8,9],
+    [8,7,6,5,4,3,2,1],
+    [],
+    [5,5,5,5],
+    ["one", "two", "three", "four", "five", "six", "seven"]
+]
+
+for A in unsorted_arrays:
+    B = A.copy()
+    print(B)
+    quick_sort(B)
+    print(B)
+    C = A.copy()
+    quick_sort(C, get_last_element)
+    assert(B == C)
+    C = A.copy()
+    quick_sort(C, get_middle_element)
+    assert(B == C)
