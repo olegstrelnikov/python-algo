4caafa3bcd7e79cfb7168061cfb54e81ba84ee3b
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2020-05-04 23:41:04 +0400

    added postgresql table creation

--- large_postgres.py
+++ large_postgres.py
@@ -10,9 +10,38 @@ def test_database(host, user, password, database):
     cur = conn.cursor()
     cur.execute("SELECT 1 FROM pg_database WHERE datname=%s", (database,))
     if cur.rowcount == 0:
+        isolation_level = conn.isolation_level
         conn.set_isolation_level(
             psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
         cur.execute("CREATE DATABASE {}".format(database))
+        conn.set_isolation_level(isolation_level)
+    conn.close()
+    conn = psycopg2.connect(host=host, user=user, password=password,
+                            dbname=database)
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE EXTENSION IF NOT EXISTS pgcrypto;
+        CREATE TABLE IF NOT EXISTS large_conversations
+        (
+            guid UUID NOT NULL DEFAULT gen_random_uuid(),
+            host_from character varying,
+            host_to character varying,
+            ip_from inet,
+            ip_to inet,
+            service character varying,
+            inbound double precision,
+            outbound double precision,
+            attrs jsonb,
+            CONSTRAINT pk_conversations PRIMARY KEY(guid),
+            CONSTRAINT uq_converstions UNIQUE
+                (host_from, host_to, ip_from, ip_to, service),
+            CONSTRAINT ck_inbound CHECK(inbound >= 0.0),
+            CONSTRAINT ck_outbound CHECK(outbound >= 0.0)
+        );
+        DELETE FROM large_conversations;
+        """
+                )
+    conn.commit()
 
 
 test_database("192.168.1.84", "jtac", "jtac", "large")
