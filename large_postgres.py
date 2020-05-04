a5f48f414837be19c7934b1e927267ed784e9780
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2020-05-05 02:16:46 +0400

    implemented db records generator

--- large_postgres.py
+++ large_postgres.py
@@ -1,9 +1,30 @@
 """ Test postgresql table with ~100M records / 2.5G in size """
 
+import random
+import string
 import psycopg2
 
 
-def test_database(host, user, password, database):
+def generate_host(length):
+    """Generate random host name"""
+    return "".join(random.choice(string.ascii_lowercase)
+                   for _ in range(length))
+
+
+def generate_record(tlds, services, ip_addresses):
+    """Generate conversations db table record"""
+    for tld1 in tlds:
+        for tld2 in tlds:
+            for ip1 in range(ip_addresses):
+                for service in services:
+                    yield (generate_host(5) + "." + tld1,
+                           generate_host(5) + "." + tld2,
+                           ip1, random.randrange(0, ip_addresses),
+                           service, random.uniform(0.0, 1000.0),
+                           random.uniform(0.0, 1000.0))
+
+
+def test_database(host, user, password, database, records):
     """Fills in database table and executes aggregate query on it"""
     conn = psycopg2.connect("host='{}' user='{}' password='{}'"
                             .format(host, user, password))
@@ -43,5 +64,16 @@ def test_database(host, user, password, database):
                 )
     conn.commit()
 
+    ip_addresses = 2**24
+    services = ("http", "smb", "cifs", "nfs", "scsi", "voip", "dns",
+                "dns", "https", "ftp", "iscsi", "amazon")
+    tlds = ("com", "net", "org", "us", "ru")
 
-test_database("192.168.1.84", "jtac", "jtac", "large")
+    assert records <= ip_addresses*len(services)*len(tlds)*len(tlds)
+    gen = generate_record(tlds, services, ip_addresses)
+    for _ in range(records):
+        record = next(gen)
+        print(record)
+
+
+test_database("192.168.1.84", "jtac", "jtac", "large", 50)
