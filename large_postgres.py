7b232fc504e7b385e9c02e019c844c3a1e709245
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2020-05-14 14:38:50 +0400

    define prepared query with maximum number of parameters

--- large_postgres.py
+++ large_postgres.py
@@ -4,7 +4,7 @@ import random
 import string
 import time
 import ipaddress
-import psycopg2.extras
+import psycopg2
 
 
 def generate_host(length):
@@ -34,6 +34,31 @@ def generate_record(tlds, services, ip_addresses, hosts_num, base):
                            random.uniform(0.0, 1000.0))
 
 
+def generate_values(tlds, services, ip_addresses, hosts_num, base):
+    """Generate conversations db table record"""
+    tlds_num = len(tlds)
+    hosts = [
+        (tlds_num*(1 + 2*i),
+         tlds_num*(2 + 2*i),
+         str(ipaddress.IPv4Address(
+            base + random.randrange(0, ip_addresses))),
+         1 + (i % len(services)),
+         ) for i in range(hosts_num//2)]
+
+    for tld1 in range(tlds_num):
+        for tld2 in range(tlds_num):
+            for ip1 in range(ip_addresses):
+                str_ip1 = str(ipaddress.IPv4Address(base + ip1))
+                for host1, host2, ip2, service in hosts:
+                    yield host1 + tld1
+                    yield host2 + tld2
+                    yield str_ip1
+                    yield ip2
+                    yield service
+                    yield random.uniform(0.0, 1000.0)
+                    yield random.uniform(0.0, 1000.0)
+
+
 def generate_table(rowcount, record):
     """Generate rowcount table records"""
     gen = generate_record(*record)
@@ -44,9 +69,15 @@ def generate_table(rowcount, record):
             return
 
 
+def generate_insert_values(records, columns):
+    """Generate SQL script"""
+    return ",".join("(" + ",".join("$"+str(i) for i in range(j*columns + 1, j*columns + columns + 1)) + ")" for j in range(records))
+
+
 def test_database(host, user, password, database, records):
     """Fills in database table and executes aggregate query on it"""
-    conn = psycopg2.connect(host=host, user=user, password=password, dbname="postgres")
+    conn = psycopg2.connect(host=host, user=user,
+                            password=password, dbname="postgres")
     cur = conn.cursor()
     cur.execute("SELECT 1 FROM pg_database WHERE datname=%s", (database,))
     if cur.rowcount == 0:
@@ -56,11 +87,13 @@ def test_database(host, user, password, database, records):
         cur.execute("CREATE DATABASE {}".format(database))
         conn.set_isolation_level(isolation_level)
     conn.close()
-    conn = psycopg2.connect(host=host, user=user, password=password, dbname=database)
+    conn = psycopg2.connect(host=host, user=user,
+                            password=password, dbname=database)
     conn.set_isolation_level(
         psycopg2.extensions.ISOLATION_LEVEL_REPEATABLE_READ)
     cur = conn.cursor()
-    cur.execute(
+    records_at_once = 65536 // 7
+    cur.execute((
         """
         CREATE EXTENSION IF NOT EXISTS pgcrypto;
         DROP TABLE IF EXISTS {0};
@@ -88,8 +121,9 @@ def test_database(host, user, password, database, records):
         PREPARE inserter AS
             INSERT INTO {0}
             (host_from, host_to, ip_from, ip_to, service, inbound, outbound)
-            VALUES ($1, $2, $3, $4, $5, $6, $7);
-        """.format("large_conversations")
+            VALUES
+        """ + generate_insert_values(records_at_once, 7) + ";")
+        .format("large_conversations")
         )
 
     ip_addresses = 2**24 - 2
@@ -98,11 +132,27 @@ def test_database(host, user, password, database, records):
     tlds = ("com", "net", "org", "us", "ru")
 
     assert records <= ip_addresses*len(services)*len(tlds)*len(tlds)
-    gen = generate_table(records,
-                         (tlds, services, ip_addresses, 100, 10*2**24 + 1))
+    gen = generate_values(tlds, services, ip_addresses, 100, 10*2**24 + 1)
     now = time.monotonic()
-    psycopg2.extras.execute_batch(
-        cur, "EXECUTE inserter (%s, %s, %s, %s, %s, %s, %s);", gen, 10000)
+    for _ in range(records // records_at_once):
+        cur.execute("EXECUTE inserter (" +
+                    "%s, "*(7*records_at_once - 1) + "%s);",
+                    [next(gen) for _ in range(7*records_at_once)])
+    records_at_once = records % records_at_once
+    if records_at_once > 0:
+        cur.execute((
+            """
+            DEALLOCATE inserter;
+            PREPARE inserter AS
+                INSERT INTO {0}
+                (host_from, host_to, ip_from, ip_to, service, inbound, outbound)
+                VALUES
+            """ + generate_insert_values(records_at_once, 7) + ";")
+            .format("large_conversations")
+            )
+        cur.execute("EXECUTE inserter (" +
+                    "%s, "*(7*records_at_once - 1) + "%s);",
+                    [next(gen) for _ in range(7*records_at_once)])
     cur.execute("DEALLOCATE inserter;")
     conn.commit()
     print(records, "records inserted --- {:.3} seconds".format(
