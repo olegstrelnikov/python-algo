2b75b4775aaf61394fc1f89d678096584ca97b71
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2020-05-21 03:23:56 +0400

    - moved rapidpg.libpq into rapidpg.Result - moved rapidpg.ExecStatusType into rapidpg.Result - moved rapidpg.ConnStatusType into rapidpg.Connection

--- rapidpg.py
+++ rapidpg.py
@@ -66,9 +66,6 @@ class Parameters:
         Parameters.librapidpg.rapidpg_add_ip4_hbo(self.parameters)
 
 
-libpq = ctypes.CDLL('libpq.so')
-
-
 class CtypesEnum(IntEnum):
     """A ctypes-compatible IntEnum superclass."""
     @classmethod
@@ -77,96 +74,46 @@ class CtypesEnum(IntEnum):
         return int(obj)
 
 
-class ExecStatusType(CtypesEnum):
-    """ enum ExecStatusType """
-
-    PGRES_EMPTY_QUERY = 0       # empty query string was executed
-
-    # a query command that doesn't return
-    # anything was executed properly by the
-    # backend
-    PGRES_COMMAND_OK = 1
-
-    # a query command that returns tuples was
-    # executed properly by the backend PGresult
-    # contains the result tuples
-    PGRES_TUPLES_OK = 2
-
-    PGRES_COPY_OUT = 3          # Copy Out data transfer in progress
-    PGRES_COPY_IN = 4           # Copy In data transfer in progress
-
-    # an unexpected response was recv'd from the
-    # backend
-    PGRES_BAD_RESPONSE = 5
-
-    PGRES_NONFATAL_ERROR = 6    # notice or warning message
-    PGRES_FATAL_ERROR = 7       # query failed
-    PGRES_COPY_BOTH = 8         # Copy In/Out data transfer in progress
-    PGRES_SINGLE_TUPLE = 9      # single tuple from larger resultset
-
-
-libpq.PQresultStatus.restype = ExecStatusType
-libpq.PQresultStatus.argtypes = [ctypes.c_void_p]
-
-libpq.PQclear.restype = None
-libpq.PQclear.argtypes = [ctypes.c_void_p]
-
-libpq.PQexec.restype = ctypes.c_void_p
-libpq.PQexec.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-
-libpq.PQconnectdbParams.restype = ctypes.c_void_p
-libpq.PQconnectdbParams.argtypes = [ctypes.POINTER(ctypes.c_char_p),
-                                    ctypes.POINTER(ctypes.c_char_p),
-                                    ctypes.c_int]
-
-
-class ConnStatusType(CtypesEnum):
-    """ enum ConnStatusType """
-    CONNECTION_OK = 0
-    CONNECTION_BAD = 1
-    # Non-blocking mode only below here
-
-    #
-    # The existence of these should never be relied upon - they should only
-    # be used for user feedback or similar purposes.
-    #
-
-    CONNECTION_STARTED = 2              # Waiting for connection to be made
-    CONNECTION_MADE = 3                 # Connection OK; waiting to send.
-    CONNECTION_AWAITING_RESPONSE = 4    # Waiting for a response from the
-                                        # postmaster.
-    CONNECTION_AUTH_OK = 5              # Received authentication; waiting for
-                                        # backend startup.
-    CONNECTION_SETENV = 6               # Negotiating environment.
-    CONNECTION_SSL_STARTUP = 7          # Negotiating SSL.
-    CONNECTION_NEEDED = 8               # Internal state: connect() needed
-    CONNECTION_CHECK_WRITABLE = 9       # Check if we could make a writable
-                                        # connection.
-    CONNECTION_CONSUME = 10             # Wait for any pending message and
-                                        # consume them.
-
-
-libpq.PQstatus.restype = ConnStatusType
-libpq.PQstatus.argtypes = [ctypes.c_void_p]
-
-libpq.PQerrorMessage.restype = ctypes.c_char_p
-libpq.PQerrorMessage.argtypes = [ctypes.c_void_p]
-
-libpq.PQfinish.restype = None
-libpq.PQfinish.argtypes = [ctypes.c_void_p]
-
-libpq.PQexecPrepared.restype = ctypes.c_void_p
-libpq.PQexecPrepared.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
-                                 ctypes.c_int, ctypes.POINTER(ctypes.c_char_p),
-                                 ctypes.POINTER(ctypes.c_int),
-                                 ctypes.POINTER(ctypes.c_int), ctypes.c_int]
-
-libpq.PQresultErrorMessage.restype = ctypes.c_char_p
-libpq.PQresultErrorMessage.argtypes = [ctypes.c_void_p]
-
-
 class Result:
     """ Wrapper of PGresult* """
+
+    class ExecStatusType(CtypesEnum):
+        """ enum ExecStatusType """
+
+        PGRES_EMPTY_QUERY = 0       # empty query string was executed
+
+        # a query command that doesn't return
+        # anything was executed properly by the
+        # backend
+        PGRES_COMMAND_OK = 1
+
+        # a query command that returns tuples was
+        # executed properly by the backend PGresult
+        # contains the result tuples
+        PGRES_TUPLES_OK = 2
+
+        PGRES_COPY_OUT = 3          # Copy Out data transfer in progress
+        PGRES_COPY_IN = 4           # Copy In data transfer in progress
+
+        # an unexpected response was recv'd from the
+        # backend
+        PGRES_BAD_RESPONSE = 5
+
+        PGRES_NONFATAL_ERROR = 6    # notice or warning message
+        PGRES_FATAL_ERROR = 7       # query failed
+        PGRES_COPY_BOTH = 8         # Copy In/Out data transfer in progress
+        PGRES_SINGLE_TUPLE = 9      # single tuple from larger resultset
+
+    libpq = ctypes.CDLL('libpq.so')
+
+    libpq.PQresultStatus.restype = ExecStatusType
+    libpq.PQresultStatus.argtypes = [ctypes.c_void_p]
+
+    libpq.PQclear.restype = None
+    libpq.PQclear.argtypes = [ctypes.c_void_p]
+
+    libpq.PQresultErrorMessage.restype = ctypes.c_char_p
+    libpq.PQresultErrorMessage.argtypes = [ctypes.c_void_p]
 
     def __init__(self, result, sql):
         self.pg_result = result
@@ -174,7 +121,7 @@ class Result:
 
     def __del__(self):
         if self.pg_result is not None:
-            libpq.PQclear(self.pg_result)
+            Result.libpq.PQclear(self.pg_result)
 
     def has_result(self):
         """ is there a result """
@@ -182,19 +129,70 @@ class Result:
 
     def status(self):
         """ whether result is ok """
-        return (libpq.PQresultStatus(self.pg_result) ==
-                ExecStatusType.PGRES_COMMAND_OK)
+        return Result.libpq.PQresultStatus(
+            self.pg_result) == ExecStatusType.PGRES_COMMAND_OK
 
     def error_message(self):
         """ get result error message"""
-        return libpq.PQresultErrorMessage(self.pg_result).decode('utf-8')
+        return Result.libpq.PQresultErrorMessage(
+            self.pg_result).decode('utf-8')
 
 
 class Connection:
     """ Wrapper of PGconn* """
 
-    RAPID_PG_BINARY = ctypes.POINTER(ctypes.c_int).in_dll(libpq,
+    RAPID_PG_BINARY = ctypes.POINTER(ctypes.c_int).in_dll(Result.libpq,
                                                           'RAPID_PG_BINARY')
+
+    class ConnStatusType(CtypesEnum):
+        """ enum ConnStatusType """
+        CONNECTION_OK = 0
+        CONNECTION_BAD = 1
+        # Non-blocking mode only below here
+
+        #
+        # The existence of these should never be relied upon - they should only
+        # be used for user feedback or similar purposes.
+        #
+
+        CONNECTION_STARTED = 2              # Waiting for connection to be made
+        CONNECTION_MADE = 3                 # Connection OK; waiting to send.
+        CONNECTION_AWAITING_RESPONSE = 4    # Waiting for a response from the
+                                            # postmaster.
+        CONNECTION_AUTH_OK = 5              # Received authentication; waiting for
+                                            # backend startup.
+        CONNECTION_SETENV = 6               # Negotiating environment.
+        CONNECTION_SSL_STARTUP = 7          # Negotiating SSL.
+        CONNECTION_NEEDED = 8               # Internal state: connect() needed
+        CONNECTION_CHECK_WRITABLE = 9       # Check if we could make a writable
+                                            # connection.
+        CONNECTION_CONSUME = 10             # Wait for any pending message and
+                                            # consume them.
+
+    Result.libpq.PQstatus.restype = ConnStatusType
+    Result.libpq.PQstatus.argtypes = [ctypes.c_void_p]
+
+    Result.libpq.PQerrorMessage.restype = ctypes.c_char_p
+    Result.libpq.PQerrorMessage.argtypes = [ctypes.c_void_p]
+
+    Result.libpq.PQfinish.restype = None
+    Result.libpq.PQfinish.argtypes = [ctypes.c_void_p]
+
+    Result.libpq.PQexecPrepared.restype = ctypes.c_void_p
+    Result.libpq.PQexecPrepared.argtypes = [
+        ctypes.c_void_p, ctypes.c_char_p,
+        ctypes.c_int, ctypes.POINTER(ctypes.c_char_p),
+        ctypes.POINTER(ctypes.c_int),
+        ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+
+    Result.libpq.PQexec.restype = ctypes.c_void_p
+    Result.libpq.PQexec.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+
+    Result.libpq.PQconnectdbParams.restype = ctypes.c_void_p
+    Result.libpq.PQconnectdbParams.argtypes = [
+        ctypes.POINTER(ctypes.c_char_p),
+        ctypes.POINTER(ctypes.c_char_p),
+        ctypes.c_int]
 
     def __init__(self, conn_params):
         keys = (ctypes.c_char_p * (len(conn_params) + 1))()
@@ -203,11 +201,11 @@ class Connection:
         values[:-1] = conn_params.values()
         keys[len(conn_params)] = None
         values[len(conn_params)] = None
-        self.pg_conn = libpq.PQconnectdbParams(keys, values, 0)
+        self.pg_conn = Result.libpq.PQconnectdbParams(keys, values, 0)
 
     def __del__(self):
         if self.pg_conn is not None:
-            libpq.PQfinish(self.pg_conn)
+            Result.libpq.PQfinish(self.pg_conn)
 
     def is_connected(self):
         """ Whether connection is established """
@@ -215,20 +213,22 @@ class Connection:
 
     def status(self):
         """ Whether connection status is ok """
-        return libpq.PQstatus(self.pg_conn) == ConnStatusType.CONNECTION_OK
+        return Result.libpq.PQstatus(
+            self.pg_conn) == ConnStatusType.CONNECTION_OK
 
     def error_message(self):
         """ Get connection error message """
-        return libpq.PQerrorMessage(self.pg_conn).decode('utf-8')
+        return Result.libpq.PQerrorMessage(self.pg_conn).decode('utf-8')
 
     def exec(self, sql):
         """ execute sql w/o parameters """
-        return Result(libpq.PQexec(self.pg_conn, sql.encode('utf-8')), sql)
+        return Result(Result.libpq.PQexec(self.pg_conn, sql.encode('utf-8')),
+                      sql)
 
     def exec_prepared(self, statement, parameters):
         """ execute prepared statement """
         contents = parameters.parameters.contents
-        return Result(libpq.PQexecPrepared(
+        return Result(Result.libpq.PQexecPrepared(
             self.pg_conn,
             statement.encode('utf-8'),
             contents.size,
